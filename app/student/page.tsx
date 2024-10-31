1b39543f3145a07490ec3dc437226abd1d47786f
krhartley <314katherine@gmail.com>
2024-10-31 09:49:06 -0400

    student pages as shown in meeting

--- app/student/page.tsx
+++ app/student/page.tsx
@@ -1,13 +1,13 @@
-import { nextAction } from "@/app/actions";
 import { SubmitButton } from "@/components/submit-button";
+import { studentUpdates } from "@/app/actions";
+
 
 export default function Next({ }: { }) {
   return (
     <form className="flex-1 flex flex-col min-w-64">
       <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
-        Student
-        <SubmitButton pendingText="Next" formAction={nextAction}>
-          Next
+        <SubmitButton pendingText="loading" formAction={studentUpdates}>
+          Continue Application
         </SubmitButton>
       </div>
     </form>
