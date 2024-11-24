4b74bc35d6d0eb28f76a783c4e43e74f5e662a33
krhartley <314katherine@gmail.com>
2024-11-24 14:43:57 -0500

    added classes, research interests, and updated welcome

--- app/student/page.tsx
+++ app/student/page.tsx
@@ -1,15 +1,33 @@
 import { SubmitButton } from "@/components/submit-button";
 import { studentUpdates } from "@/app/actions";
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 
 export default function Next({ }: { }) {
   return (
-    <form className="flex-1 flex flex-col min-w-64">
-      <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
-        <SubmitButton pendingText="Loading.." formAction={studentUpdates}>
-          Continue Application
-        </SubmitButton>
-      </div>
-    </form>
+    <form>
+    <Card className="w-[350px]">
+    <CardHeader>
+      <CardTitle>Welcome to the TAAS</CardTitle>
+      <CardDescription>Applications are due December 4th.</CardDescription>
+    </CardHeader>
+    <CardContent>
+    </CardContent>
+    <CardFooter className="flex justify-between">
+      <SubmitButton pendingText="Loading.." formAction={studentUpdates}>
+      Continue
+      </SubmitButton>
+      <Button>Submit</Button>
+    </CardFooter>
+  </Card>
+  </form>
   );
 }
