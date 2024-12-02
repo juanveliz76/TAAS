158b633c9a4897aadb9edd94fff52f02b5c21204
Juan Veliz <92879629+juanveliz76@users.noreply.github.com>
2024-12-02 11:47:43 -0500

    Add files via upload

---
+++ app/professor/select-TA-preference/confirm_submit/page.tsx
@@ -0,0 +1,16 @@
+// app/thank-you/page.tsx
+const ThankYouPage = () => {
+    return (
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <div className="text-center p-8 bg-white rounded shadow-lg">
+          <h1 className="text-4xl font-bold mb-4">Thank You!</h1>
+          <p className="text-lg">
+            Your submission has been received successfully. We appreciate your input!
+          </p>
+        </div>
+      </div>
+    );
+  };
+  
+  export default ThankYouPage;
+  
